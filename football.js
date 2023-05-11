0c7df508d5c19d69c69fde72674636622def5988
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-11 09:34:03 -0700

    Updated football.js
    Updated football.js

--- football.js
+++ football.js
@@ -1,7 +1,8 @@
-// Exercise 1 page 59, 67
+// Exercise 1 page 59, Excercise 1-4 page 67
 "use strict";
 
 let teams = [
+    {code:"", name:"Select a team", plays:""},
     {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
     {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
     {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
@@ -10,27 +11,26 @@ let teams = [
 
 window.onload = function(){
     initTeamsDropdown();
-    selectTeam();
 }
+const formTeam = document.getElementById("formTeam");
 const teamsList = document.getElementById("teamsList");
 function initTeamsDropdown(){
     for(let i = 0; i < teams.length; i++){
         let theOption = new Option(teams[i].name, teams[i].code);
         teamsList.appendChild(theOption);
     }
-    // teamsList.value = "DEN"; <---Defaults select to Denver
-    teamsList.value = null; //defaults to none of the teams
+    // teamsList.value = "DEN"; //<---Defaults select to Denver
+    // teamsList.value = null; //defaults to none of the teams
 }
 
-function selectTeam(){
-    const btnSelect = document.getElementById("btnSelect");
-    btnSelect.addEventListener("click", displayTeam);
-}
-
-function displayTeam(){
-    if (teamsList.selectedIndex >= 0) {
+formTeam.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (teamsList.selectedIndex > 0) {
         let text = teamsList.options[teamsList.selectedIndex].text;
         let plays = teams[teamsList.selectedIndex].plays;
         document.getElementById("displayInfo").innerHTML = `You selected the ${text} who play in ${plays}`;
     }
-}
+    else{
+        document.getElementById("displayInfo").innerHTML = "";
+    }
+});
